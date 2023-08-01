122ae214a3e13b1f6768b8f07ceff4d831e7ff1b
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-02 00:45:18 +0530

    DB connection error after deploy resolved

--- Configuration/db.js
+++ Configuration/db.js
@@ -1,13 +1,17 @@
 // const mysql = require("mysql");
 import mysql from "mysql";
+import dotenv from "dotenv";
 
-export const db = mysql.createConnection({
-// const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "Abhishek@123Mysql",
-    database : "blogapp"
-});
-
+const dbconfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+    // SSL configuration for secure connections (required for managed databases on Render)
+    ca: process.env.RENDER_CONNECT_CA, // This is provided by Render for SSL connections
+  },
+}
+export const db = mysql.createConnection({dbconfig});
 
 // module.exports = db; 
