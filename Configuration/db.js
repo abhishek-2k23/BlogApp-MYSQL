099cd7a29a11d73be591ebbe6448fc82eee8fc3a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-02 01:01:15 +0530

    ssl code added

--- Configuration/db.js
+++ Configuration/db.js
@@ -7,11 +7,16 @@ const dbconfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-//     ssl: {
-//     // SSL configuration for secure connections (required for managed databases on Render)
-//     ca: process.env.RENDER_CONNECT_CA, // This is provided by Render for SSL connections
-//   },
 }
+
+// Check if the RENDER_CONNECT_CA environment variable is defined (running on Render)
+if (process.env.RENDER_CONNECT_CA) {
+    dbConfig.ssl = {
+      // SSL configuration for secure connections (required for managed databases on Render)
+      ca: process.env.RENDER_CONNECT_CA,
+    };
+}
+
 export const db = mysql.createConnection(dbconfig);
 
 // module.exports = db; 
