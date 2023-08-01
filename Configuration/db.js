1a059bb014fba9553b66292c792d24522958f58d
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-02 00:55:07 +0530

    db connection error

--- Configuration/db.js
+++ Configuration/db.js
@@ -1,17 +1,17 @@
 // const mysql = require("mysql");
 import mysql from "mysql";
 import dotenv from "dotenv";
-
+dotenv.config();
 const dbconfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-    // SSL configuration for secure connections (required for managed databases on Render)
-    ca: process.env.RENDER_CONNECT_CA, // This is provided by Render for SSL connections
-  },
+//     ssl: {
+//     // SSL configuration for secure connections (required for managed databases on Render)
+//     ca: process.env.RENDER_CONNECT_CA, // This is provided by Render for SSL connections
+//   },
 }
-export const db = mysql.createConnection({dbconfig});
+export const db = mysql.createConnection(dbconfig);
 
 // module.exports = db; 
