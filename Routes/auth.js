8253ebd017816eee342c82578a79b74e84d61e31
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-02 21:17:09 +0530

    test of homepage

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,6 +2,10 @@ import express from "express"
 import { register, login, logout} from "../Controllers/auth.js";
 const router = express.Router();
 
+router.get("/",(req,res) =>{
+    res.send("AUTH page working smoothly");
+});
+
 //importing the controllers
 router.post("/register",register);
 router.post("/login",login);
