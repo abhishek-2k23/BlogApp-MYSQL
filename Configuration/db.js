247f1a59848df9613dcfe090b20b9da462189dc7
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-07 23:14:19 +0530

    db configuration changed

--- Configuration/db.js
+++ Configuration/db.js
@@ -2,7 +2,12 @@
 import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
-
-export const db = mysql.createConnection(process.env.DB_URL);
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+  });
 
 // module.exports = db; 
