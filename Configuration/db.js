3a7a6d2829c5c7c31089fb92c66367b6614cb494
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-08-08 18:58:06 +0530

    Db user Modified

--- Configuration/db.js
+++ Configuration/db.js
@@ -2,6 +2,7 @@
 import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
+// const connectionURL = `mysql://root:xnOf3hn3xHhzPMqEx40o@containers-us-west-49.railway.app:5790/railway`;
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
